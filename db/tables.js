5f45df5ed8bed60fdce87d6a1a5544472d6c7459
robyng <robyngraham01@gmail.com>
2021-12-12 20:56:42 -0800

    try to source in tables.js

--- db/tables.js
+++ db/tables.js
@@ -3,6 +3,10 @@ const db = require('./connection'); // connect to db business_db
 
 //Show depts
 const dbShowDept = () => {
+    const sourceSchema = `SOURCE /schema.sql`
+    db.query(sourceSchema)
+    const sourceSeeds = `SOURCE /seeds.sql`
+    db.query(sourceSeeds)
     const sql = `SELECT * FROM department`
     db.query(sql)
   };
